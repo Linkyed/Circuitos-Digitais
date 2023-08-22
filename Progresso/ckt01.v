05557df6c51ad5d434354eedbbcd6f9fb174e358
Nalbert <88176747+Nalbertsan@users.noreply.github.com>
2023-08-22 19:42:13 -0300

    Update Display

--- Progresso/ckt01.v
+++ Progresso/ckt01.v
@@ -1,10 +1,10 @@
-module ckt01(a, b, c, d, e, f, g, MSB, B, LSB);
+module ckt01(a, b, c, d, e, f, g, MSB, B, LSB, Perm, Func);
 
-	input MSB, B, LSB;
+	input MSB, B, LSB, Perm, Func;
 	output a, b, c, d, e, f, g;
 	
 	wire notMSB, notB, notLSB;
-	wire aux_1, aux_2,aux_3,aux_4, aux_d1,mix_d;
+	wire aux_1, aux_2,aux_3,aux_4, aux_d1,mix_d, aux_a, LD, aux_d;
 	
 	not INVERTER_MSB (notMSB, MSB);
 	not INVERTER_B (notB, B);
@@ -15,18 +15,24 @@ module ckt01(a, b, c, d, e, f, g, MSB, B, LSB);
 	and POSSIBIDADE_NUM_3(aux_3, notMSB, notB, LSB);
 	and POSSIBIDADE_NUM_4(aux_4, MSB, B, notLSB);
 	
-	or MOSTRAR_A(a, aux_1, aux_4);
+	nand Ligar_DESLIGAR(LD,Perm, Func);
 	
-	and MOSTRAR_C(c, notMSB, B, LSB);
+	or POSSI_1_4(aux_a,aux_1, aux_4);
+	or MOSTRAR_A(a, aux_a, LD);
+	
+	and MOSTRAR_B(b, LD);
+	
+	or MOSTRAR_C(c, aux_2, LD);
 	
 	or SEMPRE_1(aux_d1, MSB, notMSB);
 	nor MIX_2_E_3(mix_d, aux_2, aux_3);
-	and MOSTRAR_D(d,SEMPRE_1,mix_d);
+	and LIGAR_D(aux_d, SEMPRE_1, mix_d);
+	or MOSTRAR_D(d, aux_d, LD);
 	
-	or MOSTRAR_E(e, aux_1, aux_3, aux_4);
+	or MOSTRAR_E(e, aux_1, aux_3, aux_4, LD);
 	
-	or MOSTRAR_F(f, aux_1, aux_2, aux_3);
+	or MOSTRAR_F(f, aux_1, aux_2, aux_3, LD);
 	
-	or MOSTRAR_G(g, aux_1);
+	or MOSTRAR_G(g, aux_1, LD);
 	
 endmodule 
